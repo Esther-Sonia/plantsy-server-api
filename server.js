5f3a66ccbdfedc30d114d968c3a6845d9ab80316
Esther-Sonia <esthersonia21@gmail.com>
2025-05-01 20:29:28 +0300

    fix deployment issues

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const cors = require('cors');
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
